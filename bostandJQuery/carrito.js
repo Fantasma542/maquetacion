513487866bea5a42a584ea27a27395c1e76c5838
Fantasma542 <juanangelmoldovanpop@gmail.com>
2023-08-11 15:35:54 +0300

    RetosDia7

--- bostandJQuery/carrito.js
+++ bostandJQuery/carrito.js
@@ -1,30 +1,33 @@
-$(document).ready(function() {
+
+jQuery(() => {
+    $(".addtocart").click(function() {
+      var price = parseFloat($(this).attr("data-price"));
+      var totalPriceElement = $("#total-price");
+      var currentTotal = parseFloat(totalPriceElement.text());
+      
+      currentTotal += price;
+      totalPriceElement.text(currentTotal.toFixed(2));
+  
+      var productName = $(this).closest(".card-body").find(".card-title").text();
+      var cartItemsList = $("#cart-items");
+      cartItemsList.append("<li>" + productName + " - $" + price.toFixed(2) + "</li>");
+    });
+  });
+
+
+jQuery(() =>{
     let cartItems = [];
     let totalPrice = 0;
 
-    // Función para actualizar la lista y el precio total en el carrito
-    function updateCartDisplay() {
-        const cartItemsElement = $('#cart-items');
-        const totalElement = $('#total-price');
-        cartItemsElement.empty();
-        cartItems.forEach(item => {
-            const listItem = $('<li>').text(`Precio: ${item.price.toFixed(2)}€`);
-            cartItemsElement.append(listItem);
-        });
-        totalElement.text(totalPrice.toFixed(2));
-    }
-
-    // Función para abrir el modal del carrito
     function openCartModal() {
         $('#cart-modal').modal('show');
     }
 
-    // Función para cerrar el modal del carrito
     function closeCartModal() {
         $('#cart-modal').modal('hide');
     }
 
-    // Evento para añadir al carrito
+
     $('.addtocart').on('click', function() {
         const price = parseFloat($(this).data('price'));
         if (!isNaN(price)) {
@@ -33,13 +36,11 @@ $(document).ready(function() {
         }
     });
 
-    // Evento para abrir el carrito
     $('.btn-cart').on('click', function() {
         openCartModal();
         updateCartDisplay();
     });
-
-    // Evento para cerrar el carrito
+    
     $('.close').on('click', function() {
         closeCartModal();
     });
